aa482c080092054740e2da2197ff11164dbcde4c
Digitalgeek-hack <digitalgeek287@gmail.com>
2025-01-12 18:21:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "lyUzkT4Q#dNv62829zcIYG4YkXKdyGhqcF2LPpcc6O2Qrrx9JgvQ",
 };
